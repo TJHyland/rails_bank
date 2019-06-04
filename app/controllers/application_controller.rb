267eba9aad7c845520cbe374a2f3b681df89c36e
Tyson Hyland <tyhyland@hotmail.com>
2019-06-03 20:29:45 -0600

    end of lecture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+    # def user_params
+    #   params.require(:user).permit(:email, :encrypted_password, :first_name, :last_name)
+    # end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    end
+    
+
+
 end
